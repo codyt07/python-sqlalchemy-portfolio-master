8f4a5132e11e99f56e54329aa1642d4fcc62293c
Cody Travis <89680528+codyt07@users.noreply.github.com>
2021-11-26 23:55:13 -0600

    Finished all functions!
    Finished all the functions and linking on the website. Now all that is left is the finer details on the about me and project pages.

--- app.py
+++ app.py
@@ -3,14 +3,12 @@ from flask import(render_template, redirect,
 from models import db, Projects, app
 from datetime import datetime
 
-@app.route('/', methods = ['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.form:
-        pass
     project = Projects.query.all()
     return render_template('index.html', project=project)
 
-@app.route('/project<id>', methods = ['GET', 'POST'])
+@app.route('/project/<id>', methods = ['GET', 'POST'])
 def project_detail(id):
     detail = Projects.query.get_or_404(id)
     skill = Projects.query.get_or_404(id).skills
@@ -18,38 +16,36 @@ def project_detail(id):
     return render_template('detail.html', detail=detail, view=view)
 
 
-@app.route('/add_project', methods = ['GET', 'POST'])
-def add_project():
+@app.route('/project/new', methods = ['GET', 'POST'])
+def new():
     if request.form:
         capture = request.form['date']
-        confirmed = validate_date(capture)
-        new_project = Projects(title=request.form['title'], date=confirmed,
-        description=request.form['desc'], skills = request.form['skills'],               
-        url = request.form['github'])
+        sql_happy = datetime.strptime(capture, '%Y-%m')
+        new_project = Projects(title=request.form['title'], date=sql_happy,
+            description=request.form['desc'], skills = request.form['skills'],               
+            url = request.form['github'])
         db.session.add(new_project)
         db.session.commit()
         return redirect(url_for('index'))
     else:
         return render_template('projectform.html')
 
-@app.route('/edit_project', methods = ['GET', 'POST'])
-def edit_project():
-    id = request.args.get('id')
+@app.route('/project/<id>/edit', methods = ['GET', 'POST'])
+def edit(id):
     edit = Projects.query.get_or_404(id)
-    if request.form:
-        edit.title = request.form['title']
-    
     return render_template('edit.html', edit=edit)
 
+@app.route('/project/<id>/delete', methods=['GET', 'POST'])
+def delete(id):
+    delete = Projects.query.get_or_404(id)
+    db.session.delete(delete)
+    db.session.commit()
+    return redirect(url_for('index'))
 
-def validate_date(capture):
-    try:
-            product_date_obj = datetime.strptime(capture, '%m-%d-%Y')
-            return product_date_obj
-    except ValueError:
-        print("uh oh")
-        
-        
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     db.create_all()
     app.run(debug=True, port=8000, host="127.0.0.2")

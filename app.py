8e14cccc276735bc2e02cd2461c2660868333ebd
Cody Travis <89680528+codyt07@users.noreply.github.com>
2021-12-01 20:42:42 -0600

    added dropdown functions
    Added a "For Loop" to the drop down menu to show all projects instead of just hard coded four.
    Updated the about link to go to the detail page instead of just the index.

--- app.py
+++ app.py
@@ -15,11 +15,13 @@ def project_detail(id):
     detail = Projects.query.get_or_404(id)
     skill = Projects.query.get_or_404(id).skills
     view = skill.split(", ")
-    return render_template('detail.html', detail=detail, view=view)
+    project = Projects.query.all()
+    return render_template('detail.html', detail=detail, view=view, project=project)
 
 
 @app.route('/project/new', methods = ['GET', 'POST'])
 def new():
+    project = Projects.query.all()
     if request.form:
         capture = request.form['date']
         sql_happy = datetime.strptime(capture, '%Y-%m')
@@ -30,12 +32,13 @@ def new():
         db.session.commit()
         return redirect(url_for('index'))
     else:
-        return render_template('projectform.html')
+        return render_template('projectform.html', project=project)
 
 
 @app.route('/project/<id>/edit', methods = ['GET', 'POST'])
 def edit(id):
     edit = Projects.query.get_or_404(id)
+    project = Projects.query.all()
     if request.form:
         capture_edit = request.form['date']
         sql_happy = datetime.strptime(capture_edit, '%Y-%m')
@@ -46,7 +49,7 @@ def edit(id):
         edit.url = request.form['github']
         db.session.commit()
         return redirect(url_for('index'))
-    return render_template('edit.html', edit=edit)
+    return render_template('edit.html', edit=edit, project=project)
 
 
 @app.route('/project/<id>/delete', methods=['GET', 'POST'])
@@ -58,12 +61,14 @@ def delete(id):
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    project = Projects.query.all()
+    return render_template('about.html', project=project)
 
 
 @app.errorhandler(404)
 def not_found(error):
-    return render_template('404.html', msg=error), 404
+    project = Projects.query.all()
+    return render_template('404.html', msg=error, project=project), 404
 
 
 if __name__ == '__main__':
